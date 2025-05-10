91eadbb0704c2139396a48c6f95ddedec6907062
pablobongianino <pablo.bongianino@davinci.edu.ar>
2025-05-10 17:25:13 -0300

    version1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JOptionPane.showMessageDialog(null, "Hola");
+
+	}
+
+}
